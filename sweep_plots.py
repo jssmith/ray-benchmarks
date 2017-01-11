0de756bf9589a2b3f156c241b2dcbce07f5af879
Johann Schleier-Smith <jssmith@gmail.com>
2017-01-10 20:44:08 -0800

    better colors

--- sweep_plots.py
+++ sweep_plots.py
@@ -5,10 +5,7 @@ from itertools import groupby
 from cycler import cycler
 
 
-color_sequence = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c',
-                  '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5',
-                  '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f',
-                  '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+color_sequence = ['orange', 'green', 'blue', 'cyan', 'red']
 
 def plot_all(data):
 
