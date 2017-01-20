a969646f18d8f50e2cecf19868b98b8f94a8c86c
Johann Schleier-Smith <jssmith@gmail.com>
2017-01-20 08:00:07 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# from __future__ import print_function
+
+# import os
+# import subprocess
+
+from setuptools import setup, find_packages
+
+setup(name="raybench",
+      version="0.0.1",
+      packages=find_packages(),
+      install_requires=["numpy"],
+      license="Apache 2.0")
