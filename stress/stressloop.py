6fb44268a3d272cfa3a320b8b7881d4a3cc9ee29
Johann Schleier-Smith <jssmith@gmail.com>
2017-02-23 22:50:20 -0800

    update stress script

--- stress/stressloop.py
+++ stress/stressloop.py
@@ -1,4 +1,5 @@
 import re
+import sys
 import time
 import logging
 import json
@@ -13,12 +14,6 @@ class Logger(object):
         self.f.close()
 
     def log(self, event_type, event_data):
-        # o = {
-        #     "type" : event_type,
-        #     "timestamp" : time.time(),
-        #     "data" : event_data
-        #     }
-        # print(o)
         json.dump({
             "type" : event_type,
             "timestamp" : time.time(),
@@ -60,8 +55,8 @@ class StressRay(object):
             self.container_id = container_id
             return container_id
 
-    def run_benchmark(self):
-        proc = Popen(["docker", "exec", self.container_id, "python", "/benchmark/workloads/matmul_ray.py"], stdout=PIPE)
+    def run_benchmark(self, workload_script):
+        proc = Popen(["docker", "exec", self.container_id, "python", workload_script], stdout=PIPE)
         (stdoutdata, stderrdata) = proc.communicate()
         print stdoutdata
         print stderrdata
@@ -78,31 +73,78 @@ class StressRay(object):
         if not stopped_container_id or self.container_id != stopped_container_id:
             raise RuntimeError("Failed to stop container {}".format(self.container_id))
 
-if __name__ == "__main__":
-    logger = Logger("log.txt")
-    s = StressRay(logger)
-    container_id = s.start_ray()
-    print "started container", container_id
-    time.sleep(2)
-    sequential_failures = 0
-    for i in range(10000):
-        logger.log("start_work", {
+    def _do_iteration(self, iteration_state):
+        i = iteration_state.iteration_index
+        iteration_state.iteration_index += 1
+        self.logger.log("start_work", {
             "iteration" : i
             })
         start_time = time.time()
-        success = s.run_benchmark()
+        success = self.run_benchmark(workload_script)
         elapsed_time = time.time() - start_time
-        logger.log("finish_work", {
+        self.logger.log("finish_work", {
                 "iteration" : i,
                 "elapsed_time" : elapsed_time,
                 "success" : success
             })
 
         if success:
-            sequential_failures = 0
+            iteration_state.num_successes += 1
+            iteration_state.sequential_failures = 0
+            continue_iteration = True
         else:
-            sequential_failures += 1
-            if sequential_failures >= 3:
-                print "excessive failures"
+            iteration_state.num_failures += 1
+            iteration_state.sequential_failures += 1
+            if iteration_state.sequential_failures >= 3:
+                iteration_state.iteration_end_reason = "excessive_failures"
+                continue_iteration = False
+            else:
+                continue_iteration = True
+        return continue_iteration
+
+    def iterate_workload(self, workload_script, iteration_target=None, time_target=None):
+        class iteration_state: pass
+        iteration_state.sequential_failures = 0
+        iteration_state.excessive_failures = False
+        iteration_state.iteration_index = 0
+        iteration_state.num_successes = 0
+        iteration_state.num_failures = 0
+        iteration_state.iteration_start_time = time.time()
+        iteration_state.iteration_end_reason = "target_reached"
+
+        loop_predicate_iteration = lambda: iteration_state.iteration_index < iteration_target
+        loop_predicate_time = lambda: time.time() - iteration_state.iteration_start_time < time_target
+        if iteration_target and not time_target:
+            loop_predicate = loop_predicate_iteration
+        elif time_target and not iteration_target:
+            loop_predicate = loop_predicate_time
+        else:
+            loop_predicate = lambda: loop_predicate_time() and loop_predicate_iteration()
+
+        while loop_predicate():
+            if not self._do_iteration(iteration_state):
                 break
+        iteration_state.iteration_end_time = time.time()
+
+        logger.log("finish_iteration", {
+            "workload_script" : workload_script,
+            "excessive_failures" : iteration_state.excessive_failures,
+            "num_successes" : iteration_state.num_successes,
+            "num_failures" : iteration_state.num_failures,
+            "elapsed_time" : iteration_state.iteration_end_time - iteration_state.iteration_start_time,
+            "iteration_end_reason" : iteration_state.iteration_end_reason
+            })
+
+if __name__ == "__main__":
+    workload_script = sys.argv[1]
+    logger = Logger("log.txt")
+    s = StressRay(logger)
+    container_id = s.start_ray()
+
+    # sleep a little bit to give Ray time to start
+    time.sleep(2)
+
+    # s.iterate_workload(workload_script, iteration_target=10)
+    s.iterate_workload(workload_script, time_target=20)
+
     s.stop_ray()
