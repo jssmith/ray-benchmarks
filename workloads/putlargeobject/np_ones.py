7477a319e78a432984f297d15689740693f44b5c
Johann Schleier-Smith <jssmith@gmail.com>
2017-03-03 15:07:32 -0800

    more

--- workloads/putlargeobject/np_ones.py
+++ workloads/putlargeobject/np_ones.py
@@ -2,20 +2,9 @@ import json
 import ray
 import raybench
 
+import numpy as np
 
 import raybench.eventstats as eventstats
-
-def dotest(n):
-    with eventstats.BenchmarkLogSpan("string"):
-        x = n * "a"
-        ray.put(x)
-    with eventstats.BenchmarkLogSpan("array_of_pairs"):
-        x = {i:i for i in range(n)}
-        ray.put(x)
-    with eventstats.BenchmarkLogSpan("ntuple"):
-        x = n * (1,)
-        ray.put(x)
-
 
 if __name__ == '__main__':
     bench_env = raybench.Env()
@@ -23,7 +12,9 @@ if __name__ == '__main__':
 
     n = int(10 ** bench_env.benchmark_iteration)
 
-    dotest(n)
+    with eventstats.BenchmarkLogSpan("np.ones"):
+        x = np.ones(n)
+        ray.put(x)
 
     print "BENCHMARK_STATS:", json.dumps({
         "config": { "scale" : n },
